f3f36d9a63a3a1afc5367275ed35dc4c2b9b33e0
thr13 <skhrnt2945@gmail.com>
2024-06-19 11:21:52 +0900

    fixed: Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -29,14 +29,15 @@ const SearchBar = ({ searchMeeting, setSearchMeeting }) => {
 const Hashtags = ({ hashtags }) => {
     return (
         <div className="main__hashtags">
-            {hashtags.map((tag) => (
-                <div key={tag.hashtag_id}>{tag.hashtag_name}</div>
+            {
+                hashtags.map((tag) => (
+                <div key = {tag.hashtag_id}>{tag.hashtag_name}</div>
             ))}
         </div>
     );
 };
 
-// 게시물 길이가 길면 잘라줌
+// 게시물 길이가 길면 ... 으로 표현
 const cutContent = (content, maxLength) => {
     if (content.length <= maxLength) {
         return content;
@@ -47,10 +48,11 @@ const cutContent = (content, maxLength) => {
 // 게시물
 const Posts = ({ posts = [], title }) => {
     const navigate = useNavigate();
+    // 게시물 클릭 이벤트
     const handlePostClick = (postId) => {
-        navigate('/party', { state: { postId } });
+        navigate('/party', { state: { postId } }); // 'party'페이지에 객체 postId 값을 전달
     };
-    // 게시물이 한 개도 없을 때 출력
+    // 게시물이 한 개도 없을 때 출력 메시지
     if (posts.length === 0) {
         return <div className="main__posts-empty">게시물이 없습니다.</div>;
     }
@@ -58,7 +60,8 @@ const Posts = ({ posts = [], title }) => {
     return (
         <div className="main__posts">
             <h2 className="main__posts-title">{title}</h2>
-            {posts.map((post) => (
+            {
+                posts.map((post) => (
                 <div key={post.id} className="main__post" onClick={() => handlePostClick(post.id)}>
                     {/*조건문을 포스트에 이미지가 없으면 기본 이미지 설정할것 */}
                     <img src={cat} alt="default" className="main__post-default" />
@@ -80,39 +83,31 @@ const Main = () => {
     const [allPosts, setAllPosts] = useState([]);
     const [hashtags, setHashtags] = useState([]);
     const [page] = useState(1);
-    const [size] = useState(10);
+    const [size] = useState(100);
 
     const navigate = useNavigate();
-    const location = useLocation();
+
 
     const fetchPosts = async (loginStatus) => {
         try {
-            const allPostsUrl = `/posts?page=${page}&size=${size}`;
-            const allPostsResponse = await api.get(allPostsUrl, {
-                withCredentials: true
-            });
-
+            // 전체 게시물
+            const allPostsResponse = await api.get(`/posts?page=${page}&size=${size}`, {withCredentials: true});
             const allPostResponses = allPostsResponse.data.no_login_posts_responses.content;
 
             let PostResponses = [];
             let hashtagDtoList = [];
 
-
+            // 로그인 되었을때, 해시태그 추천 게시물
             if (loginStatus) {
-                const PostsUrl = `/posts/login?page=${page}&size=${size}`;
-                const taggedPostsResponse = await api.get(PostsUrl, {
-                    withCredentials: true
-                });
-                PostResponses = taggedPostsResponse.data.login_posts_responses.content;
-                hashtagDtoList = taggedPostsResponse.data.hashtag_dto_list || [];
+                const hashtagPostsResponse = await api.get(`/posts/login?page=${page}&size=${size}`, {withCredentials: true });
+                PostResponses = hashtagPostsResponse.data.login_posts_responses.content;
+                hashtagDtoList = hashtagPostsResponse.data.hashtag_dto_list || [];
             } else {
                 hashtagDtoList = allPostsResponse.data.hashtag_dto_list || [];
             }
-
             setPosts(PostResponses);
             setAllPosts(allPostResponses);
             setHashtags(hashtagDtoList);
-
         } catch (error) {
             console.error('게시물 에러:', error);
         }
@@ -120,10 +115,10 @@ const Main = () => {
 
     useEffect(() => {
         if (isLogin !== null) {
-            console.log(`isLogin 상태: ${isLogin}`);
+            console.log(`isLogin 상태: ${isLogin}`); //나중에 지울것
             fetchPosts(isLogin);
         }
-    }, [isLogin, location.state, page, size]);
+    }, [isLogin]);
 
     const handleNewPost = () => {
         navigate('/newpost');
